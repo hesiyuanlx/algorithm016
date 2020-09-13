6a72dd31a6b5b8fad78cd5e19d95a2873a5e80d3
hesiyuan <305181582@qq.com>
2020-09-13 23:17:18 +0800

    添加

---
+++ Week_01/MoveZeroes.java
@@ -0,0 +1,21 @@
+package com.hsy.stu.algorithm.practice.arraytype;
+
+public class MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int currentNotZeroNumIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[currentNotZeroNumIndex] = nums[i];
+                currentNotZeroNumIndex += 1;
+                continue;
+            }
+        }
+        for (int j = currentNotZeroNumIndex; j < nums.length; j++) {
+            nums[j] = 0;
+        }
+    }
+}
